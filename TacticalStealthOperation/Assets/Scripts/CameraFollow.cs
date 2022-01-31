c07a0727687565568f303b787b4839086213fca4
lelogeaisy <lelogeaisy@eisti.eu>
2022-01-31 16:26:09 +0100

    ajout de l'herbe, d'une minimap, modification des batiments pour leur donner une unicite, ajout des ventilations dans les batiments d'alex, ajout d'arbres et d'élements entre les batiments

--- TacticalStealthOperation/Assets/Scripts/CameraFollow.cs
+++ TacticalStealthOperation/Assets/Scripts/CameraFollow.cs
@@ -4,14 +4,17 @@ using Vector3 = UnityEngine.Vector3;
 public class CameraFollow : MonoBehaviour {
     [SerializeField] public Transform target;
     [SerializeField] private float smoothSpeed = 0.125f;
+    [SerializeField] private float offsetY = 10.0f;
+    [SerializeField] private float offsetZ = -2.72f;
+    [SerializeField] private float inclinaison;
     private Vector3 vec;
     void Start(){
-        vec = new Vector3(0, 8.43f, -2.72f);
-        transform.eulerAngles = new Vector3(67.41f, 0, 0);
+        vec = new Vector3(0, offsetY, offsetZ);
+        transform.eulerAngles = new Vector3(inclinaison, 0, 0);
     }
     
     void FixedUpdate() {
         Vector3 desiredPosition = target.position+vec;
-        transform.position += (desiredPosition-transform.position)*0.125f;
+        transform.position += (desiredPosition-transform.position)*smoothSpeed;
     }
 }
