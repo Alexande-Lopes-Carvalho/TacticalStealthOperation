b14b9e024df642bf1849bddc020f354b6c16eafb
lelogeaisy <lelogeaisy@eisti.eu>
2022-02-06 17:46:36 +0100

    ajout menu pause, menu victoire, correction inventaire level 1

--- TacticalStealthOperation/Assets/Scripts/Utility/LevelManager.cs
+++ TacticalStealthOperation/Assets/Scripts/Utility/LevelManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
@@ -12,13 +14,30 @@ public class LevelManager : MonoBehaviour
     public Canvas LoseUICanvas{
         get => loseUICanvas;
     }
+    [SerializeField] private Canvas winUICanvas;
+    public Canvas WinUICanvas{
+        get => winUICanvas;
+    }
+    [SerializeField] private Canvas pausedUICanvas;
     [SerializeField] private Transform levelParent;
+
+    private float pausedVal = 0.0f;
+    private float playVal = 1.0f;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            PauseOrResume();
+        }
+    }
 
     public void SetLevel(int index){
         if (index >= 0 && index < levelPrefabs.Count){
             mainCamera.SetActive(false);
             mainCanvas.gameObject.SetActive(false);
             loseUICanvas.gameObject.SetActive(false);
+            winUICanvas.gameObject.SetActive(false);
+            pausedUICanvas.gameObject.SetActive(false);
             Instantiate(levelPrefabs[index],levelParent);
             levelPrefabs[index].SetActive(true);
             Index = index;
@@ -34,12 +53,39 @@ public class LevelManager : MonoBehaviour
         DestroyLevels();
         mainCamera.SetActive(true);
         loseUICanvas.gameObject.SetActive(false);
+        winUICanvas.gameObject.SetActive(false);
         mainCanvas.gameObject.SetActive(true);
+        Time.timeScale = playVal;
     }
 
     public void DestroyLevels(){
         for(int i=0;i<levelParent.childCount;++i){
             Destroy(levelParent.GetChild(i).gameObject);
         }
+    }
+
+    public void PauseOrResume()
+    {
+        if (Time.timeScale == pausedVal)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+        
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = pausedVal;
+        pausedUICanvas.gameObject.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = playVal;
+        pausedUICanvas.gameObject.SetActive(false);
     }
 }
---
+++ TacticalStealthOperation/Assets/Scripts/Utility/WinDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class WinDisplay : MonoBehaviour
+{
+    private LevelManager levelManager {get;set;}
+    public void Start()
+    {
+        levelManager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
+    }
+
+    public void OnTriggerEnter(Collider coll)
+    {
+        Debug.Log("collision");
+        if (coll.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("player");
+            levelManager.Pause();
+            levelManager.WinUICanvas.gameObject.SetActive(true);
+        }
+    }
+}
